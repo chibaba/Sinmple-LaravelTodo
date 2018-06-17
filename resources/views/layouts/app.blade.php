bc52a931d6ba1109d2f109c08f2e3013050b983e
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-06-17 20:06:10 +0100

    added Sass compilation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>TodoList</title>
+<link rel="stylesheet" href="/css/app.css"
+</head>
+<body>
+<div class="container">
+@yield('content')
+</div>
+<footer id="footer">
+<p>copyright &copy; 2018</p>
+</footer>
+</body>
+</html>
